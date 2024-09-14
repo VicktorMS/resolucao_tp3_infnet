53d3b4e9702c49289a196361c3f12a687608be50
victormoraes <victor.moraesrj@gmail.com>
2024-09-14 10:40:52 -0300

    feat: added mini_projeto_2 concluded

--- mini_projeto_2/app.py
+++ mini_projeto_2/app.py
@@ -1,4 +1,3 @@
-#"ID_do_Produto,Nome_do_Produto,Quantidade,Valor_Unitário",
 transacoes = [
     "001,Teclado Gamer,2,150.00",
     "002,Mouse Sem Fio,5,75.50",
@@ -12,17 +11,49 @@ transacoes = [
     "010,Placa de Vídeo RTX 3060,1,4500.00"
 ]
 
-maior_venda = 0
-idx_maior_venda = 0
-for i in range(len(transacoes)):
-    transacao = transacoes[i].split(",")
-    valor_total = int(transacao[2]) * float(transacao[3])
-    if valor_total > maior_venda:
-        maior_venda = valor_total
-        idx_maior_venda = i
-    transacao.append(str(valor_total))
-    transacoes[i] = ",".join(transacao)
+def calcular_vendas(transacoes):
+    vendas_totais = []
+    for transacao in transacoes:
+        id_produto, nome_produto, quantidade, valor_unitario = transacao.split(",")
+        valor_total = int(quantidade) * float(valor_unitario)
+        vendas_totais.append((id_produto, nome_produto, int(quantidade), valor_total))
+    return vendas_totais
 
+def converter_moeda(vendas_totais, fator_conversao):
+    vendas_convertidas = []
+    for venda in vendas_totais:
+        _, nome_produto, _, valor_total = venda
+        valor_convertido = valor_total * fator_conversao
+        vendas_convertidas.append((nome_produto, valor_convertido))
+    return vendas_convertidas
 
-print(transacoes)
-print(transacoes[idx_maior_venda])
+def produto_mais_vendido(vendas_totais):
+    mais_vendido = vendas_totais[0] 
+    for venda in vendas_totais:
+        if venda[2] > mais_vendido[2]:  
+            mais_vendido = venda
+    return mais_vendido
+
+def produto_maior_receita(vendas_totais):
+    maior_receita = vendas_totais[0]
+    for venda in vendas_totais:
+        if venda[3] > maior_receita[3]:  
+            maior_receita = venda
+    return maior_receita
+
+vendas_totais = calcular_vendas(transacoes)
+mais_vendido = produto_mais_vendido(vendas_totais)
+maior_receita = produto_maior_receita(vendas_totais)
+
+fator_conversao = float(input("Digite o fator de conversão para a nova moeda: "))
+vendas_convertidas = converter_moeda(vendas_totais, fator_conversao)
+
+for venda in vendas_totais:
+    print(f"Produto: {venda[1]}, Quantidade Vendida: {venda[2]}, Valor Total: {venda[3]:.2f}")
+
+print(f"\nProduto mais vendido: {mais_vendido[1]} (Quantidade: {mais_vendido[2]})")
+print(f"Produto com maior receita: {maior_receita[1]} (Receita: R${maior_receita[3]:.2f})\n\n")
+print("="*5, "Valores com conversão de Moeda", "="*5, '\n')
+for venda in vendas_convertidas:
+    nome_produto, valor_convertido = venda
+    print(f"Produto: {nome_produto}, Valor de Vendas totais Convertido: {valor_convertido:.2f}")
